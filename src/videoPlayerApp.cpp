143729f61987188d91c0d11cd803f327dcf63ffd
James Hughes <james@virtualjames.com>
2014-05-19 16:01:06 -0700

    changed to loading movie by string

--- src/videoPlayerApp.cpp
+++ src/videoPlayerApp.cpp
@@ -35,10 +35,16 @@ void videoPlayerApp::setup()
     if (dir.exists()) {
         dir.listDir();
         dir.sort();
-        files = dir.getFiles();
-        if (files.size() > 0) {
+        std::vector<ofFile> dir_files = dir.getFiles();
+
+        for (auto& file: dir_files) {
+            files[file.getFileName()] = file;
+            ofLogVerbose("-- file map added: " + file.getFileName());
+        }
+
+        if (dir_files.size() > 0) {
             ofxOMXPlayerSettings player_settings;
-            player_settings.videoPath = files[0].path();
+            player_settings.videoPath = dir_files[0].path();
             player_settings.useHDMIForAudio = true;                         //default true
             player_settings.enableLooping = config.player_enable_looping;   //default true
             player_settings.doFlipTexture = config.player_flip_texture;
@@ -47,11 +53,9 @@ void videoPlayerApp::setup()
 
             front_player = ofxOMXPlayerPtr(new ofxOMXPlayer);
             front_player->setup(player_settings);
-            front_index = 0;
 
             back_player = ofxOMXPlayerPtr(new ofxOMXPlayer);
             back_player->setup(player_settings);
-            back_index = 0;
             back_player->setPaused(true);
         } else {
             ofLogError("Videos directory does not exist: " + dir.getAbsolutePath());
@@ -62,37 +66,44 @@ void videoPlayerApp::setup()
         ofExit(1);
     }
 
+    loader.start(*this);
+
     blankScreen();
 }
 
 void videoPlayerApp::run()
 {
-    while(!back_lock.tryWriteLock())
-        Poco::Thread::yield();
+    ofFile file;
 
-    back_player->loadMovie(files[back_index].path());
-    back_player->stepFrameForward();
-    back_player->setPaused(false);
+    while(true) {
+        if (file_queue.pop(file)) {
+            ofLogVerbose("-- loading file: " + file.path());
 
-    while(!front_lock.tryWriteLock())
-        Poco::Thread::yield();
+            back_player->loadMovie(file.path());
+            back_player->stepFrameForward();
+            back_player->setPaused(false);
 
-    swap(back_player, front_player);
-    front_lock.unlock();
-    back_lock.unlock();
-    front_index = back_index.load();
-    if (screen_blanked) screen_blanked = false;
+            while(!front_lock.tryWriteLock())
+                Poco::Thread::yield();
 
-}
-void videoPlayerApp::loadMovie(uint_fast8_t index)
-{
-    if (index > files.size()-1) {
-        ofLogError("!! File index out of bounds !!");
-        return;
+            swap(back_player, front_player);
+            front_lock.unlock();
+            back_player->setPaused(true);
+
+            if (screen_blanked) screen_blanked = false;
+        } else {
+            Poco::Thread::yield();
+        }
     }
-
-    back_index = index;
-    pool.start(*this);
+}
+void videoPlayerApp::loadMovie(std::string file_name)
+{
+    try {
+        file_queue.push(files.at(file_name));
+    }
+    catch (const std::out_of_range& oor) {
+        ofLogError("Out of range error: " + file_name);
+    }
 }
 
 //--------------------------------------------------------------
@@ -102,13 +113,28 @@ void videoPlayerApp::update()
         ofxOscMessage m;
         osc_receiver.getNextMessage( &m );
 
-        if ( m.getAddress() == "/loadMovie" ) {
-            int i = m.getArgAsInt32(0);
-            loadMovie(i);
+        if (m.getAddress() == "/blankScreen" ) {
+            ofLogVerbose("got message: /blankScreen");
+            blankScreen();
         }
 
-        if (m.getAddress() == "/blankScreen" ) {
-            blankScreen();
+        if ( m.getAddress() == "/debug" ) {
+            ofLogVerbose("got message: /debug");
+            debug = !debug;
+        }
+
+        if ( m.getAddress() == "/fullScreen" ) {
+            ofLogVerbose("got message: /fullScreen");
+            ofToggleFullscreen();
+        }
+
+        if ( m.getAddress() == "/loadMovie" ) {
+            string file_name = m.getArgAsString(0);
+            ofLogVerbose("got message: /loadMovie " + file_name);
+            if (0 == file_name.compare("blankScreen"))
+                blankScreen();
+            else
+                loadMovie(file_name);
         }
     }
 }
@@ -120,10 +146,10 @@ void videoPlayerApp::draw(){
         ofBackground(ofColor::black);
     } else {
         front_lock.readLock();
-        front_player->draw(0, 0, ofGetWidth(), ofGetHeight());
+        front_player->draw(0, 0);
         if (debug) {
             stringstream info;
-            info << "\n" <<  "CURRENT MOVIE: " << files[front_index].path();
+            info << "\n" <<  "CURRENT MOVIE: " << front_player->settings.videoPath;
             ofDrawBitmapStringHighlight(front_player->getInfo() + info.str(), 60, 60, ofColor(ofColor::black, 90), ofColor::yellow);
         }
         front_lock.unlock();
@@ -135,36 +161,36 @@ void videoPlayerApp::keyPressed  (int key){
 
     ofLogVerbose(__func__) << "key: " << key;
     switch (key) {
-    case '0':
-        loadMovie(0);
-        break;
-    case '1':
-        loadMovie(1);
-        break;
-    case '2':
-        loadMovie(2);
-        break;
-    case '3':
-        loadMovie(3);
-        break;
-    case '4':
-        loadMovie(4);
-        break;
-    case '5':
-        loadMovie(5);
-        break;
-    case '6':
-        loadMovie(6);
-        break;
-    case '7':
-        loadMovie(7);
-        break;
-    case '8':
-        loadMovie(8);
-        break;
-    case '9':
-        loadMovie(9);
-        break;
+    // case '0':
+    //     loadMovie(0);
+    //     break;
+    // case '1':
+    //     loadMovie(1);
+    //     break;
+    // case '2':
+    //     loadMovie(2);
+    //     break;
+    // case '3':
+    //     loadMovie(3);
+    //     break;
+    // case '4':
+    //     loadMovie(4);
+    //     break;
+    // case '5':
+    //     loadMovie(5);
+    //     break;
+    // case '6':
+    //     loadMovie(6);
+    //     break;
+    // case '7':
+    //     loadMovie(7);
+    //     break;
+    // case '8':
+    //     loadMovie(8);
+    //     break;
+    // case '9':
+    //     loadMovie(9);
+    //     break;
     case 'b':
         blankScreen();
         break;
--- src/videoPlayerApp.h
+++ src/videoPlayerApp.h
@@ -1,12 +1,14 @@
 #pragma once
 
 #include <atomic>
+#include <map>
 #include "ConsoleListener.h"
 #include "ofMain.h"
 #include "ofxOMXPlayer.h"
 #include "ofxOsc.h"
 #include "Poco/RWLock.h"
-#include "Poco/ThreadPool.h"
+#include "Poco/Thread.h"
+#include "RingBuffer.h"
 
 struct videoPlayerAppConfig {
     int           window_width;
@@ -27,16 +29,14 @@ class videoPlayerApp
       public Poco::Runnable
 {
     typedef ofPtr<ofxOMXPlayer> ofxOMXPlayerPtr;
+    typedef RingBuffer<ofFile, 2> FileQueue;
 
 public:
 
     videoPlayerApp(videoPlayerAppConfig _config)
         : config(_config),
           screen_blanked(true),
-          debug(false),
-          front_index(0),
-          back_index(0),
-          pool(1)
+          debug(false)
     {};
 
     void setup();
@@ -46,22 +46,20 @@ public:
     void onVideoEnd(ofxOMXPlayerListenerEventData& e);
     void onVideoLoop(ofxOMXPlayerListenerEventData& e){ /*empty*/ };
     void onCharacterReceived(SSHKeyListenerEventData& e);
-    void loadMovie(uint_fast8_t index);
+    void loadMovie(std::string file_name);
     void blankScreen();
     void run();
 
-    videoPlayerAppConfig  config;
+    videoPlayerAppConfig    config;
     atomic<bool>            screen_blanked;
     bool                    debug;
     ofxOMXPlayerPtr         front_player;
-    atomic<uint_fast8_t>    front_index;
     ofxOMXPlayerPtr         back_player;
-    atomic<uint_fast8_t>    back_index;
-    vector<ofFile>          files;
+    std::map<std::string, ofFile> files;
+    FileQueue               file_queue;
     ConsoleListener         consoleListener;
-    Poco::ThreadPool        pool;
+    Poco::Thread            loader;
     Poco::RWLock            front_lock;
-    Poco::RWLock            back_lock;
 
 
     ofxOscReceiver          osc_receiver;
